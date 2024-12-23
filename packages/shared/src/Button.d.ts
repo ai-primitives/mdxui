42bce3c50ef7080197d2c17d1e84135edc9a10f0
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2024-12-23 07:11:54 -0600

    fix: incorrect reference

--- packages/shared/src/Button.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-export interface ButtonProps {
-  children: React.ReactNode
-  onClick?: () => void
-}
-export declare const Button: React.FC<ButtonProps>
-//# sourceMappingURL=Button.d.ts.map
