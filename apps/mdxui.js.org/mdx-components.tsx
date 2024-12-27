3a3878d9eaa0fcf47e65b48a44a4eca1a75a33f2
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2024-12-27 11:21:43 -0600

    fix: mdx components types

--- apps/mdxui.js.org/mdx-components.tsx
+++ apps/mdxui.js.org/mdx-components.tsx
@@ -1,7 +1,9 @@
+import { useMDXComponents as getDocsMDXComponents } from 'nextra-theme-docs'
 import type { MDXComponents } from 'mdx/types'
- 
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    ...components,
-  }
-}
+
+const docsComponents = getDocsMDXComponents()
+
+export const useMDXComponents = (components: MDXComponents) => ({
+  ...docsComponents,
+  ...components,
+})
